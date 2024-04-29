e19fba71597fb014e623cabbd37c5bd9880830ec
Al Nahian Rafi <153551470+dev-nahianrafi@users.noreply.github.com>
2024-04-29 06:05:11 +0600

    homePge

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import Error from "./pages/Error/Error";
 import RootLayout from "./components/Layout/RootLayout";
 import Home from "./pages/Home/Home";
 import Registration from "./pages/auth/Registration";
+import Settings from "./pages/setting/Settings";
+import Message from "./pages/message/Message";
+import Notification from "./pages/notification/Notification";
 
 
 function App() {
@@ -19,6 +22,9 @@ function App() {
           <Route path="/registration" element={<Registration/>}/>
           <Route element={<RootLayout/>}>
             <Route path="/home" element={<Home/>}/>
+            <Route path="/message" element={<Message/>}/>
+            <Route path="/notification" element={<Notification/>}/>
+            <Route path="/settings" element={<Settings/>}/>
           </Route>
             <Route path="*" element={<Error/>}/>
         </Route>
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,59 @@
+import { styled } from "@mui/material";
+import "./sidebar.css"
+import Avatar from '@mui/material/Avatar';
+import { LuLogOut } from "react-icons/lu";
+import { IoHomeOutline } from "react-icons/io5";
+import { AiOutlineMessage } from "react-icons/ai";
+import { IoSettingsOutline } from "react-icons/io5";
+import { FaRegBell } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
 
+
+const Logout = styled(LuLogOut)({
+  fontSize: "46px",
+  color: "#fff"
+})
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className="sidebar_main">
+      <div className="sidebar_inner">
+        <div className="sidebarProfile">
+            <Avatar
+              alt="Remy Sharp"
+              src="/static/images/avatar/1.jpg"
+              sx={{ width: 90, height: 90 }}
+            />
+        </div>
+        <div className="sidebarItem">
+            <ul>
+              <li>
+                <NavLink to="/home">
+                  <IoHomeOutline />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/message">
+                  <AiOutlineMessage />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/notification">
+                  <FaRegBell />
+                </NavLink>
+              </li>
+              <li>
+                <NavLink to="/settings">
+                  <IoSettingsOutline />
+                </NavLink>
+              </li>
+            </ul>
+        </div>
+        <div className="sidebarLogout">
+          <Logout style={{cursor: 'pointer'}}/>
+        </div>
+      </div>
+    </div>
   )
 }
 
