54a5e2bea7b501ef3180c9013502295c6b0739f5
Al Nahian Rafi <153551470+dev-nahianrafi@users.noreply.github.com>
2024-05-19 04:13:27 +0600

    formikValidationdone

--- src/pages/auth/Registration.jsx
+++ src/pages/auth/Registration.jsx
@@ -14,9 +14,11 @@ import RadioGroup from '@mui/material/RadioGroup';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import FormControl from '@mui/material/FormControl';
 import FormLabel from '@mui/material/FormLabel';
-import TextField from '@mui/material/TextField';
 import { LiaEyeSolid } from "react-icons/lia";
 import { useState } from 'react';
+import { useFormik } from 'formik';
+import Registrationvalidation from '../../validation/Registration';
+
 
 
 const Headinglogin = styled(Typography)({
@@ -30,7 +32,7 @@ const Headinglogin = styled(Typography)({
 const Hide = styled(LiaEyeSolid)({
   fontSize: "20px",
   position: "absolute",
-  top: "278px",
+  top: "10px",
   right: "8px",
   cursor: "pointer"
 })
@@ -54,7 +56,6 @@ const BootstrapButton = styled(Button)({
 const Registration = () => {
 
   let [show,setShow] = useState(true)
-
   let handleShow = () =>{
     if(show){
       setShow(false)
@@ -62,6 +63,23 @@ const Registration = () => {
       setShow(true)
     }
   }
+
+  const initialValuesreg = {
+    email: '',
+    name: '',
+    gender: '',
+    password: ''
+  }
+
+  const formik = useFormik({
+    initialValues: initialValuesreg,
+    validationSchema: Registrationvalidation,
+
+    onSubmit: values => {
+      console.log(values);
+      // alert(JSON.stringify(values, null, 2));
+    },
+  });
   
   return (
     <Box sx={{ flexGrow: 1 }}>
@@ -73,34 +91,82 @@ const Registration = () => {
               </Headinglogin>
               <p style={{color: '#11175D', fontSize:"20px", opacity:"0.5", marginBottom:"40px"}}>Free register and you can enjoy it</p>
 
-              <div className="inputboxes">
-                  <Inputbox variant="standard" placeholder="Email Address"/>
-                  <Inputbox variant="standard" placeholder="Full Name"/>
-                  <FormControl>
-                      <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
-                      <RadioGroup
-                        row
-                        aria-labelledby="demo-row-radio-buttons-group-label"
-                        name="row-radio-buttons-group"
-                      >
-                        <FormControlLabel value="female" control={<Radio />} label="Female" />
-                        <FormControlLabel value="male" control={<Radio />} label="Male" />
-                        <FormControlLabel value="other" control={<Radio />} label="Other" />
-                      </RadioGroup>
-                 </FormControl>
-                  
-                 <TextField
-                    id="standard-password-input"
-                    label="Password"
-                    type={show ? "password" : "text"}
-                    autoComplete="current-password"
-                    variant="standard"
-                  />
-                  <Hide onClick={handleShow} />
-              </div>
-              <BootstrapButton variant="contained" disableRipple>
-                  Sign up
-              </BootstrapButton>
+              <form action="#" onSubmit={formik.handleSubmit}>
+                <div className="inputboxes">
+                  <div>
+                    <Inputbox 
+                      variant="standard" 
+                      placeholder="Email Address"
+                      id="email"
+                      name="email"
+                      type="email"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                    />
+                    {formik.touched.email && formik.errors.email ? (
+                    <p style={{color: 'red', marginTop:"10px"}}>{formik.errors.email}</p>
+                    ) : null}
+                  </div>
+                  <div>
+                      <Inputbox 
+                        variant="standard" 
+                        placeholder="Full Name"
+                        id="name"
+                        type="text"
+                        onChange={formik.handleChange}
+                        value={formik.values.name}
+                      />
+                      {formik.touched.name && formik.errors.name ? (
+                      <p style={{color: 'red', marginTop:"10px"}}>{formik.errors.name}</p>
+                      ) : null}
+                  </div>
+                  <div>
+                    <FormControl>
+                        <FormLabel id="demo-row-radio-buttons-group-label">Gender</FormLabel>
+                        <RadioGroup
+                          row
+                          aria-labelledby="demo-row-radio-buttons-group-label"
+                          name="row-radio-buttons-group"
+                        >
+                          <FormControlLabel 
+                          value="female" 
+                          control={<Radio />} 
+                          label="Female" 
+                          />
+                          <FormControlLabel 
+                          value="female" 
+                          control={<Radio />} 
+                          label="Female" 
+                          />
+                          <FormControlLabel 
+                          value="female" 
+                          control={<Radio />} 
+                          label="Male" 
+                          />
+                        </RadioGroup>
+                    </FormControl>
+                  </div>                 
+                  <div className='hideshow'>
+                    <Inputbox
+                        variant="standard"
+                        placeholder="Password"
+                        type={show ? "password" : "text"}
+                        autoComplete="current-password"
+                        id="password"
+                        name="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                      />
+                      {formik.touched.password && formik.errors.password ? (
+                      <p style={{color: 'red', marginTop:"10px"}}>{formik.errors.password}</p>
+                      ) : null}
+                    <Hide onClick={handleShow} />
+                  </div>
+                </div>
+                <BootstrapButton type="submit" variant="contained" disableRipple>
+                    Sign up
+                </BootstrapButton>
+              </form>
               <span style={{color:"#03014C", fontSize:"14px"}}>Already  have an account ?  <a href="/" style={{color:"#EA6C00"}}>Log In</a></span>
           </div>
 
---
+++ src/validation/Registration.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import React from 'react'
+import * as Yup from 'yup';
+
+
+const emailregex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+const Registrationvalidation = Yup.object({
+    name: Yup.string()
+      .max(20, 'Must be 15 characters or less')
+      .required('Required'),
+    // lastName: Yup.string()
+    //   .max(20, 'Must be 20 characters or less')
+    //   .required('Required'),
+    email: Yup.string()
+          .email('Invalid email address')
+          .matches(emailregex, 'Enter Your Right Email*')
+          .required('Required*'),
+
+    password: Yup.string()
+            .max(15, 'Max 15')
+            .min(8,'Enter At Least 8 Charecter')
+            .required('Please Enter Your Password')
+  })
+
+// const Registration = () => {
+//   return (
+//     <div>Registration</div>
+//   )
+// }
+
+export default Registrationvalidation
